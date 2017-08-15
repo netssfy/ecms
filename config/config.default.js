44840561e0a7ff694da91569c464c63d6fb7198b
chunyuan.wu <chunyuan.wu@dianrong.com>
2017-08-15 15:50:51 +0800

    init egg

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1502782889039_6750';
+
+  // add your config here
+
+  return config;
+};
